126697c3d07de6cd8f34369107079081b49a1ac9
loknopf <lorenz.knopf@t-online.de>
2026-05-17 18:29:46 +0200

    refactor: unified fixture runners for valid and invalid fixtures;

--- tests/diagnostics.rs
+++ tests/diagnostics.rs
@@ -1,29 +1,36 @@
 use std::path::PathBuf;
 
-fn fixture(name: &str) -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("fixtures/invalid")
-        .join(name)
-}
-
-fn check_snapshot(snapshot_name: &str, fixture_name: &str) {
-    let output = latch_rs::check(fixture(fixture_name));
-    let mut settings = insta::Settings::clone_current();
-    settings.add_filter(r"[^\s]*/fixtures/", "fixtures/");
-    settings.bind(|| insta::assert_snapshot!(snapshot_name, output));
+#[test]
+fn run_valid_fixtures() {
+    let valid_fixtures_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("fixtures/valid");
+    for valid_fixture in std::fs::read_dir(valid_fixtures_dir).unwrap() {
+        let valid_fixture = valid_fixture.unwrap();
+        let name = valid_fixture.file_name();
+        dbg!(format!(
+            "Running fixture {} ({})",
+            name.to_str().unwrap(),
+            valid_fixture.path().to_str().unwrap()
+        ));
+        let output = latch_rs::check(valid_fixture.path());
+        assert_eq!(output, "Check done - 0 errors found");
+    }
 }
 
 #[test]
-fn field_name_collision() {
-    check_snapshot("field_name_collision", "field_name_collision.latch");
-}
-
-#[test]
-fn field_overlap() {
-    check_snapshot("field_overlap", "field_overlap.latch");
-}
-
-#[test]
-fn parse_error_missing_name() {
-    check_snapshot("parse_error_missing_name", "parse_error.latch");
+fn run_invalid_fixtures() {
+    let valid_fixtures_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("fixtures/invalid");
+    for valid_fixture in std::fs::read_dir(valid_fixtures_dir).unwrap() {
+        let valid_fixture = valid_fixture.unwrap();
+        let name = valid_fixture.file_name();
+        let snapshot_name = name.to_str().unwrap().strip_suffix(".latch").unwrap();
+        dbg!(format!(
+            "Running fixture {} ({})",
+            name.to_str().unwrap(),
+            valid_fixture.path().to_str().unwrap()
+        ));
+        let output = latch_rs::check(valid_fixture.path());
+        let mut settings = insta::Settings::clone_current();
+        settings.add_filter(r"[^\s]*/fixtures/", "fixtures/");
+        settings.bind(|| insta::assert_snapshot!(snapshot_name, output));
+    }
 }
